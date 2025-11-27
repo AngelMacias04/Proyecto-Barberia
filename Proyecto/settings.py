68158b232c671c10bbad2f20148d6ea4df2f94b8
Angel Erdene Macias Gomez <erdene@Angels-MacBook-Pro.local>
2025-11-27 12:11:57 -0600

    Aceptar conexiones remotas dentro de la misma red

--- Proyecto/settings.py
+++ Proyecto/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-qm99$m@o)vo%$&)xm(yq&6u%cubky)(my^r+*(ygxpki_d=lo+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'barberia',
+    'import_export',
 ]
 
 DEBUG = True
--- barberia/admin.py
+++ barberia/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
+from import_export.admin import ImportExportModelAdmin
+
 from .models import Barbero
 from .models import Citas
 from .models import Cliente
@@ -6,12 +8,17 @@ from .models import Duenio
 from .models import Pagos
 from .models import Resultados
 from .models import Servicios
-# Register your models here.
 
-admin.site.register(Barbero)
-admin.site.register(Citas)
-admin.site.register(Cliente)
-admin.site.register(Duenio)
-admin.site.register(Pagos)
-admin.site.register(Resultados)
-admin.site.register(Servicios)
+
+class BaseImportExportAdmin(ImportExportModelAdmin):
+    """Enable import/export options for every registered model."""
+    pass
+
+
+admin.site.register(Barbero, BaseImportExportAdmin)
+admin.site.register(Citas, BaseImportExportAdmin)
+admin.site.register(Cliente, BaseImportExportAdmin)
+admin.site.register(Duenio, BaseImportExportAdmin)
+admin.site.register(Pagos, BaseImportExportAdmin)
+admin.site.register(Resultados, BaseImportExportAdmin)
+admin.site.register(Servicios, BaseImportExportAdmin)
